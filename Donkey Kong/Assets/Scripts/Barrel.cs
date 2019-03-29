8e99de11337746e5d61b4ca0fd45da19439391bc
Nico Hickman <nicohman@demenses.net>
2019-03-29 16:38:55 -0700

    Add basic lives and oil tank

--- Donkey Kong/Assets/Scripts/Barrel.cs
+++ Donkey Kong/Assets/Scripts/Barrel.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Barrel : MonoBehaviour
 {
 	private Rigidbody2D rigid;
 	public float speed = 1;
-	private int going = 1;
+	public  int going = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +21,9 @@ public class Barrel : MonoBehaviour
     void OnCollisionEnter2D(Collision2D col) {
     	if (col.gameObject.tag == "Border") {
     		going *= -1;
+    	} else if (col.gameObject.tag == "Player") {
+    		col.gameObject.GetComponent<PlayerController>().ModLives(-1);
+    		SceneManager.LoadScene("BaseGame");
     	}
     }
 }
