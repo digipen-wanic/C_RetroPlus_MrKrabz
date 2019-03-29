4e7bf92b4af77ad10b243bcd0b5098d7647a4f46
Nico Hickman <nicohman@demenses.net>
2019-03-29 16:28:04 -0700

    Add basic barrel and fix platform collisions

---
+++ Donkey Kong/Assets/Scripts/Barrel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+	private Rigidbody2D rigid;
+	public float speed = 1;
+	private int going = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = new Vector2(speed * going, rigid.velocity.y);
+    }
+    void OnCollisionEnter2D(Collision2D col) {
+    	if (col.gameObject.tag == "Border") {
+    		going *= -1;
+    	}
+    }
+}
